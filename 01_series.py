64f2c8524e2bcb70ae84e75e46b309ccf76a05e5
Pravin Yadav <pravinsy2003@gmail.com>
2026-03-26 12:28:22 +0530

    updated series file

--- 01_series.py
+++ 01_series.py
@@ -1,3 +1,3 @@
 import pandas as pd
 df = pd.Series([1,2,3])          # it will create a single column with index starting with 0
-print(df)
+print(df)                        # df is a variable name which we can use later to perform some actions like print it 
